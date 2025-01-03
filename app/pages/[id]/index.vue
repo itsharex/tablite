4df3002989498d7f6d18f3190cb18b64af39dc58
Tamago <zekun.jin@foxmail.com>
2025-01-03 15:55:31 +0800

    chore: rename pages

--- app/pages/[id]/index.vue
+++ app/pages/[id]/index.vue
@@ -2,10 +2,16 @@
 import ArrowLeft from '~icons/heroicons/arrow-left'
 import TableCells from '~icons/heroicons/table-cells'
 
+interface Position {
+  x: number
+  y: number
+}
+
 const router = useRouter()
-const cnxId = useRouteParams<string>('cnxId')
+const cnxId = useRouteParams<string>('id')
 const { tables } = useTables(cnxId)
 const selectedTable = ref('')
+const selectedCell = ref<Partial<Position>>({})
 const { data, columns, execute } = useTable(selectedTable, cnxId)
 
 async function onSelectTable(t: string) {
@@ -13,6 +19,10 @@ async function onSelectTable(t: string) {
     return
   selectedTable.value = t
   await execute()
+}
+
+async function onSelectCell(x: number, y: number) {
+  selectedCell.value = { x, y }
 }
 </script>
 
@@ -26,11 +36,13 @@ async function onSelectTable(t: string) {
             <span class="text-xl font-semibold">Tables</span>
           </div>
 
-          <div class="px-4 my-4">
+          <div class="px-4 mt-4">
             <Input class="h-8" placeholder="Search Tables" />
           </div>
 
-          <div class="h-full overflow-y-auto pb-4">
+          <div class="h-full relative overflow-y-auto pb-4">
+            <div class="sticky top-0 h-4 bg-gradient-to-b from-zinc-50 to-transparent" />
+
             <div v-for="table in tables" :key="table" class="flex items-center justify-between text-sm gap-1.5 min-h-8 px-4 cursor-default text-zinc-600" :class="[selectedTable === table ? 'bg-zinc-200' : 'hover:bg-zinc-200/50']" @click="onSelectTable(table)">
               <TableCells class="flex-shrink-0" />
               <div class="flex-1 truncate">
@@ -43,14 +55,18 @@ async function onSelectTable(t: string) {
 
       <ResizableHandle />
 
-      <ResizablePanel class="relative overflow-auto flex flex-col w-full bg-white">
-        <div class="grid flex-1 gap-px overflow-auto -mr-px text-xs bg-zinc-100 border-y border-zinc-100" :style="{ gridTemplateColumns: `repeat(${columns.length}, 200px)` }">
-          <div v-for="col in columns" :key="col.Field" class="sticky top-0 h-8 px-3 flex items-center font-semibold bg-zinc-50 shadow">
-            {{ col.Field }}
+      <ResizablePanel class="bg-white">
+        <div v-show="columns.length" class="w-full h-full overflow-auto grid flex-1 gap-px text-xs cursor-default bg-zinc-100 border-zinc-100" :style="{ gridTemplateColumns: Array.from({ length: columns.length }, () => '200px').join(' ') }">
+          <div v-for="col in columns" :key="col.Field" class="sticky -mt-24 -top-24 h-32 bg-zinc-50 shadow">
+            <div class="h-8 px-3 flex items-center font-semibold mt-24">
+              {{ col.Field }}
+            </div>
           </div>
           <template v-for="(row, y) in data">
-            <div v-for="(col, x) in columns" :key="`${x}:${y}`" class="h-8 px-3 flex items-center truncate" :class="y % 2 ? 'bg-zinc-50' : 'bg-white'">
-              {{ row[col.Field] }}
+            <div v-for="(col, x) in columns" :key="`${x}:${y}`" class="h-8 px-3 flex items-center hover:bg-zinc-200/50" :class="y % 2 ? 'bg-zinc-50' : 'bg-white'" @dblclick="onSelectCell(x, y)">
+              <div class="truncate">
+                {{ row[col.Field] }}
+              </div>
             </div>
           </template>
         </div>
