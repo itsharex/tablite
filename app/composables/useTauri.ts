5ff8d9859e1e75fd42a0c0f23bba032b2f65457d
Tamago <zekun.jin@foxmail.com>
2025-01-08 16:58:10 +0800

    feat: use less table limit

--- app/composables/useTauri.ts
+++ app/composables/useTauri.ts
@@ -1,7 +1,7 @@
 import { getCurrentWindow } from '@tauri-apps/api/window'
 
-export function useTauriStorage<T = unknown>(key: string, initialValue: MaybeRefOrGetter<T>) {
-  return useStorageAsync(key, initialValue, createTrauriStorage('store.json'))
+export function useTauriStorage<T = unknown>(key: string, initialValue: MaybeRefOrGetter<T>, file: string = 'store.json') {
+  return useStorageAsync(key, initialValue, createTrauriStorage(file))
 }
 
 export function useTauriWindow() {
