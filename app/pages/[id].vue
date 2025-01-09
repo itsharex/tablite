f0617762f5c3c0abb856fa6c5547a121c7e6a950
Tamago <zekun.jin@foxmail.com>
2025-01-09 17:50:10 +0800

    chore: update tabs size

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -42,8 +42,8 @@ provide('__TABLITE:CURSOR', instance)
 
 <template>
   <div class="h-screen -mt-7 flex">
-    <div class="w-[70px] flex flex-col gap-2.5 flex-shrink-0 border-r py-12 bg-zinc-800 border-r-zinc-200">
-      <div v-for="tab in tabs" :key="tab.key" class="flex items-center cursor-pointer justify-center size-10 mx-auto rounded-md" :class="[route.name === tab.key ? 'text-white bg-white/15' : 'text-white/50 hover:text-white/75']" @click="router.replace({ name: tab.key })">
+    <div class="w-[70px] flex flex-col gap-1 flex-shrink-0 border-r py-12 bg-zinc-800 border-r-zinc-200">
+      <div v-for="tab in tabs" :key="tab.key" class="flex items-center cursor-pointer justify-center size-[44px] mx-auto rounded-md" :class="[route.name === tab.key ? 'text-white bg-white/15' : 'text-white/50 hover:text-white/75']" @click="router.replace({ name: tab.key })">
         <component :is="tab.icon" class="flex-shrink-0 size-5" />
       </div>
     </div>
