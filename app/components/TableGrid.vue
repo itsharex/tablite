2869659163062d59439c9b28eb465d550c12463d
Tamago <zekun.jin@foxmail.com>
2025-01-14 10:26:52 +0800

    feat: use div in table grid

--- app/components/TableGrid.vue
+++ app/components/TableGrid.vue
@@ -44,7 +44,7 @@ const rowVirtualizerOptions = computed(() => {
     count: rows.value?.length ?? 0,
     estimateSize: () => 32,
     getScrollElement: () => domRef.value,
-    overscan: 50,
+    overscan: 10,
   }
 })
 
@@ -81,49 +81,34 @@ function isBlob(value: any) {
 <template>
   <div ref="domRef" class="w-full overflow-auto flex-1 relative">
     <div :style="{ height: `${totalSize}px` }">
-      <table class="grid text-xs">
-        <thead class="grid sticky top-0 z-10">
-          <tr
-            v-for="headerGroup in table?.getHeaderGroups()"
-            :key="headerGroup.id"
-            class="flex w-full gap-px"
-          >
-            <th
+      <div class="relative text-xs">
+        <div class="flex gap-px sticky top-0 z-10">
+          <template v-for="headerGroup in table?.getHeaderGroups()" :key="headerGroup.id">
+            <div
               v-for="header in headerGroup.headers"
               :key="header.id"
-              :colspan="header.colSpan"
               :style="{ width: `${header.getSize()}px` }"
-              class="text-start h-8 flex items-center px-3 font-semibold shadow cursor-default bg-zinc-50"
+              class="flex-shrink-0 text-start h-8 flex items-center px-3 font-semibold shadow cursor-default bg-zinc-50"
             >
               <FlexRender :render="header.column.columnDef.header" :props="header.getContext()" />
-            </th>
-          </tr>
-        </thead>
+            </div>
+          </template>
+        </div>
 
-        <tbody
-          :style="{ height: `${totalSize}px` }"
-          class="grid relative"
-        >
-          <tr
-            v-for="vRow in virtualRows"
-            :ref="measureElement /*measure dynamic row height*/"
-            :key="rows[vRow.index]!.id"
-            :data-index="vRow.index"
-            class="flex absolute w-full gap-px"
-            :style="{ transform: `translateY(${vRow.start}px)` }"
-          >
-            <td
+        <div class="relative -mt-px" :style="{ height: `${totalSize}px` }">
+          <div v-for="vRow in virtualRows" :ref="measureElement" :key="rows[vRow.index]!.id" :data-index="vRow.index" class="flex absolute w-full gap-px h-8 box-border border-t border-zinc-100" :style="{ transform: `translateY(${vRow.start}px)` }">
+            <div
               v-for="cell in rows[vRow.index]!.getVisibleCells()"
               :key="cell.id"
               :style="{ width: `${cell.column.getSize()}px` }"
-              class="flex w-0 justify-start px-3 h-8 items-center cursor-default text-zinc-800 hover:bg-zinc-200/50"
+              class="flex flex-shrink-0 w-0 justify-start px-3 h-full items-center cursor-default text-zinc-800 hover:bg-zinc-200/50"
               :class="[vRow.index % 2 ? 'bg-zinc-50' : 'bg-white']"
             >
               <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
