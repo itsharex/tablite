c71fc760962eff539717c5257276caa80dbcef8c
Tamago <zekun.jin@foxmail.com>
2025-01-14 17:18:43 +0800

    chore: selectable cell

--- app/components/VirtualGridBodyCell.vue
+++ app/components/VirtualGridBodyCell.vue
@@ -8,14 +8,14 @@ const props = defineProps<{
   isSelected?: boolean
 }>()
 
-const emit = defineEmits(['click'])
+const emit = defineEmits(['click', 'dblclick'])
 
 const isBlob = computed(() => props.dataType?.includes('blob') || Array.isArray(props.value))
 const isEmpty = computed(() => ['', undefined, null].includes(props.value))
 
 const BlobCell = <Badge class="origin-left scale-75 text-xs">BLOB</Badge>
 const EmptyCell = <input readonly class="placeholder-zinc-600/50 focus-visible:outline-none bg-transparent w-full h-full" placeholder="EMPTY" />
-const DefaultCell = <span class="truncate select-text cursor-auto">{props.value}</span>
+const DefaultCell = <span class="truncate cursor-default">{props.value}</span>
 
 const component = computed(() => {
   if (isBlob.value)
@@ -27,8 +27,8 @@ const component = computed(() => {
 </script>
 
 <template>
-  <div class="w-full h-full flex items-center px-3 box-border relative" @click="emit('click')">
-    <component :is="component" class="z-[1]" />
+  <div class="w-full h-full flex items-center px-3 box-border relative" :class="{ 'select-text': isSelected }" @click="emit('click')">
+    <component :is="component" class="z-[1]" :class="{ 'cursor-text': isSelected }" />
     <div v-if="isSelected" v-motion-fade class="absolute inset-0 border-2 border-zinc-600" />
   </div>
 </template>
