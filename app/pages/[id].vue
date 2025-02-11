ab0ae5934dd1e31590ec8e29888691bdc20f9c11
Tamago <zekun.jin@foxmail.com>
2025-02-11 16:11:23 +0800

    feat: add hyper text

--- app/pages/[id].vue
+++ app/pages/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { platform } from '@tauri-apps/plugin-os'
 import { hash } from 'ohash'
+import ChevronUpDown from '~icons/heroicons/chevron-up-down'
 import CircleStack from '~icons/heroicons/circle-stack'
 import CodeBracket from '~icons/heroicons/code-bracket'
 import Cog6Tooth from '~icons/heroicons/cog-6-tooth'
@@ -63,7 +64,7 @@ preloadRouteComponents({ name: 'id-queries' })
   <div class="h-screen flex flex-col" :class="[IS_MACOS ? '-mt-12' : '-mt-8']">
     <div class="w-full p-2 flex justify-between items-center flex-shrink-0 bg-zinc-50" :class="[IS_MACOS ? 'h-12' : 'h-8']">
       <div class="flex items-center h-full box-border" :class="{ 'pl-[72px]': IS_MACOS }">
-        <Button v-if="IS_MACOS" variant="ghost" size="sm" class="z-[101] font-semibold px-4 uppercase align-middle text-zinc-600 hover:bg-zinc-200/50" @click="router.replace({ name: 'index' })">
+        <Button v-if="IS_MACOS" variant="ghost" size="sm" class="z-[101] font-semibold px-4 uppercase align-middle hover:bg-zinc-200/50" @click="router.replace({ name: 'index' })">
           <span>TABLITE</span>
           <span class="-translate-y-px">/</span>
           <span class="text-zinc-600/50">{{ db }}</span>
@@ -75,13 +76,13 @@ preloadRouteComponents({ name: 'id-queries' })
         </div>
       </div>
 
-      <Button v-if="llm" variant="ghost" size="sm" class="z-[101] h-8 text-zinc-600 hover:bg-zinc-200/50">
+      <Button v-if="llm" variant="ghost" size="sm" class="z-[101] h-8 hover:bg-zinc-200/50" @click="router.replace({ name: 'id-settings' })">
         <img :src="llm.icon" class="size-4">
-
-        <span>{{ llm.alias ?? llm.model }}</span>
+        <HyperText :text="llm.alias ?? llm.model" :duration="300" class="cursor-pointer" />
+        <ChevronUpDown class="size-4" />
       </Button>
 
-      <Button v-else variant="ghost" size="sm" class="z-[101] h-8 w-8 p-0 text-zinc-600 hover:bg-zinc-200/50" @click="router.replace({ name: 'id-settings' })">
+      <Button v-else variant="ghost" size="sm" class="z-[101] h-8 w-8 p-0 hover:bg-zinc-200/50" @click="router.replace({ name: 'id-settings' })">
         <Sparkles />
       </Button>
     </div>
