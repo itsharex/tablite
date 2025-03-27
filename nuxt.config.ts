317a9cf38c770b3cbd50d96c791940b57012a521
Tamago <zekun.jin@foxmail.com>
2025-03-27 15:15:46 +0800

    chore: upgrade deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import process from 'node:process'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
 
   ssr: false,
 
