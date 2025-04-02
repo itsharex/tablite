5211bec257bd7912eb96e90e7ce89c28e82c062d
Tamago <zekun.jin@foxmail.com>
2025-04-02 10:16:26 +0800

    feat: use tauri native traffic light position

--- src-tauri/crates/tauri-plugin-sql-extra/permissions/autogenerated/reference.md
+++ src-tauri/crates/tauri-plugin-sql-extra/permissions/autogenerated/reference.md
@@ -2,6 +2,8 @@
 
 Default permissions for the plugin
 
+#### This default permission set includes the following:
+
 - `allow-close`
 - `allow-load`
 - `allow-select`
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,23 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-use tauri_plugin_decorum::WebviewWindowExt;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_decorum::init())
         .plugin(tauri_plugin_sql_extra::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .setup(|app| {
-            let window = app.get_webview_window("main").unwrap();
-            window.create_overlay_titlebar().unwrap();
-
-            #[cfg(target_os = "macos")]
-            window.set_traffic_lights_inset(15.0, 23.0).unwrap();
-
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
